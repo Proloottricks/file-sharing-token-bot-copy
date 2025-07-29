cf95bc3f6ed6abcf519d3f47db46932bbff04f24
Proloottricks <ritiksakhala17@gmail.com>
2025-07-29 23:41:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "adrinolinks.in")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "5822cda99a97a9f8f39d9aefd247031fd1513e57")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/FirstWatchTutorial/2") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/HowToWatchVedio/4") # shareus ka tut_vid he 
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002314480478"))
