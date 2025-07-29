1b3dc909950f00a20d60672c23a0d2055b6b9bfd
Proloottricks <ritiksakhala17@gmail.com>
2025-07-29 19:51:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6376864232").split()):
+    for x in (os.environ.get("ADMINS", "1809208278").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -77,7 +77,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6695586027)
+ADMINS.append(1809208278)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
